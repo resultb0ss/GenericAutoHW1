6e81c274e87754a0d581f00015bcd665b91989c8
Admin <reteamboss@gmail.com>
2024-08-26 10:33:49 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arrayOne = {1,2,3,4,5};
+        int[] arrayTwo = {0,34,43};
+
+
+        boolean equalResult = equalArray(arrayOne,arrayTwo);
+
+
+        if (equalResult == true){
+            System.out.println("Массивы равны");
+        } else {
+            System.out.println("Массивы не равны");
+        }
+
+
+    }
+
+    public static boolean equalArray (int[] arrayOne, int[] arrayTwo){
+        boolean result = true;
+        for (int i = 0; i < arrayOne.length; i++){
+            if (arrayOne[i] != arrayTwo[i]){
+                result = false;
+                break;
+            }
+        } return result;
     }
 }
